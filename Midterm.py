e3f3103d61a258a6343be075c30fb8025f8733a9
Laila Persico <2484641@edu.vaniercollege.qc.ca>
2025-10-21 08:32:03 -0400

    Update Midterm.py 4

--- Midterm.py
+++ Midterm.py
@@ -3,10 +3,32 @@ import math
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
     return 0.0
+    result = 0
+    result += round((math.pi) * (math.pow(r**2)), 2)
+    return result
+print(area_of_circle(5))
+            
 
 # Q2: Hollow Right Triangle
+# *         
+# **
+# * *
+# ****
+
 def hollow_right_triangle(n):
     return ""
+result = ""
+
+for i in range(n):
+    if i == 0:
+            result += "*" + "\n"
+    else:
+            result += ("*" + (" " * (i-1))) + "\n"
+    return result.rstrip()
+
+print(hollow_right_triangle(4))
+
+
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
