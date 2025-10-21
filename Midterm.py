5b4764e0128536b7dd2915b52db7d73498b3e21e
Laila Persico <2484641@edu.vaniercollege.qc.ca>
2025-10-21 09:43:37 -0400

    Update Midterm.py 4

--- Midterm.py
+++ Midterm.py
@@ -2,28 +2,37 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
-    result = 0
-    result += round((math.pi) * (math.pow(r**2)), 2)
-    return result
-print(area_of_circle(5))
+    result = 0.0
+    
+    for i in range(radius):
+
+        result += round((math.pi) * (math.pow(radius, 2)), 2)
+    
+        return result
+
+print(area_of_circle(3))
             
 
 # Q2: Hollow Right Triangle
-# *         
+# *
 # **
 # * *
 # ****
 
-def hollow_right_triangle(n):
-    return ""
-result = ""
+def hollow_right_triangle(n): 
+    result = ""
 
-for i in range(n):
-    if i == 0:
+    for i in range(n):
+        if n < 4:
+            result += "The triangle height should be at least 4." + "\n"
+        elif i == 0:
             result += "*" + "\n"
-    else:
-            result += ("*" + (" " * (i-1))) + "\n"
+        elif i == n-1:
+            result += ("*" * n) + "\n"
+        else:
+            result += ("*" + (" " * (i-1)) + "*") + "\n"
+        
+
     return result.rstrip()
 
 print(hollow_right_triangle(4))
@@ -31,8 +40,26 @@ print(hollow_right_triangle(4))
 
 
 # Q3: Inverted Pyramid
-def inverted_pyramid(n):
-    return ""
+#*****
+# ***
+#  *
+def inverted_pyramid(n): 
+
+    result = ""
+
+    for i in range(n):
+        if n < 3:
+            result += "The pyramid height should be at least 3." + "\n"
+        elif i == n:
+            result += "*"
+        else:
+            result += (" " * i)
+            result += ("*" * (n-i)) 
+            result += ("*" * (n- (i+1))) + "\n"
+
+    return result.rstrip()
+
+print(inverted_pyramid(3))
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
